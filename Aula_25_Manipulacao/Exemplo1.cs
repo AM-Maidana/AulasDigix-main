d4f99751349638524f7762bc70c3f4344a0abb18
AM-Maidana <amanda.maidana@outlook.com>
2025-01-15 16:55:15 -0400

    Exemplos e exercicios de manipulação de arquivos

---
+++ Aula_25_Manipulacao/Exemplo1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula25
+{
+    public class Exemplo1
+    {
+        // static void Main(string [] args)
+        // {
+        //     string filePath = "dados.bin"; // Caminho do arquivo
+
+        //     using(BinaryWriter writer = new 
+        //         BinaryWriter(File.Open(filePath, FileMode.Create)))
+        //     {
+        //         writer.Write(123); // Colocando o valor 123 no arquivo
+        //         writer.Write(45.3546); 
+        //         writer.Write("Hello World");
+        //     }
+
+        //     System.Console.WriteLine("Dados gravados no: " + filePath);
+        // }
+    }
+}
